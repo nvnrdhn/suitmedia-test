a3f54c2360cb6c9bd371447b83507724acff08b2
Novan Ardh <NovanArdhn_@live.com>
2020-03-16 00:26:21 +0700

    1st test

---
+++ app/src/main/java/com/nvnrdhn/suitmediatest/GuestActivity.kt
@@ -0,0 +1,12 @@
+package com.nvnrdhn.suitmediatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest)
+    }
+}
---
+++ app/src/main/java/com/nvnrdhn/suitmediatest/helper/GuestAdapter.kt
@@ -0,0 +1,2 @@
+package com.nvnrdhn.suitmediatest.helper
+
